0d53d504438d5055df113fd2cc6d07e5dbd16cd4
Pheniex <ritchie2000@yandex.ru>
2022-04-04 23:20:43 +0300

    add HealthPercentDecorator, NeedAmmoDecorator,

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -32,6 +32,7 @@ public:
 
     bool TryToAddAmmo(int32 ClipsAmount);
     bool IsAmmoEmpty() const;
+    bool IsAmmoFull() const;
 
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
@@ -65,7 +66,6 @@ protected:
 
     void DecreaseAmmo();
     bool IsClipEmpty() const;
-    bool IsAmmoFull() const;
 
     void LogAmmo();
 
--- Source/ShootThemUp/ShootThemUp.Build.cs
+++ Source/ShootThemUp/ShootThemUp.Build.cs
@@ -35,7 +35,8 @@ public class ShootThemUp : ModuleRules
             "ShootThemUp/Public/AI",
             "ShootThemUp/Public/AI/Tasks",
             "ShootThemUp/Public/AI/Services",
-            "ShootThemUp/Public/AI/EQS"
+            "ShootThemUp/Public/AI/EQS",
+            "ShootThemUp/Public/AI/Decorators"
 
         });
 
