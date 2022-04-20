30c2f7d5c582bb4b4e829f53394f677faa096424
Pheniex <ritchie2000@yandex.ru>
2022-04-20 14:58:59 +0300

    add HealthBar to AI Character

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -1,15 +1,16 @@
 // Shoot Them Up Game, by Pheniex
 
-
 #include "AI/STUAICharacter.h"
 #include "AI/STUAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Components/STUAIWeaponComponent.h"
 #include "BrainComponent.h"
+#include "Components/WidgetComponent.h"
+#include "UI/STUHealthBarWidget.h"
+#include "Components/STUHealthComponent.h"
 
-
-ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) 
-: Super(ObjInit.SetDefaultSubobjectClass<USTUAIWeaponComponent>("WeaponComponent"))
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
+    : Super(ObjInit.SetDefaultSubobjectClass<USTUAIWeaponComponent>("WeaponComponent"))
 {
     AutoPossessAI = EAutoPossessAI::Disabled;
     AIControllerClass = ASTUAIController::StaticClass();
@@ -20,9 +21,28 @@ ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
         GetCharacterMovement()->bUseControllerDesiredRotation = true;
         GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
     }
+
+    HealthWidgetComponent = CreateDefaultSubobject<UWidgetComponent>("HealthWidgetComponent");
+    HealthWidgetComponent->SetupAttachment(GetRootComponent());
+    HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+    HealthWidgetComponent->SetDrawAtDesiredSize(true);
 }
 
-void ASTUAICharacter::OnDeath() 
+void ASTUAICharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    check(HealthWidgetComponent);
+}
+
+void ASTUAICharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    UpdateHealthWidgetVisibility();
+}
+
+void ASTUAICharacter::OnDeath()
 {
     Super::OnDeath();
 
@@ -31,4 +51,30 @@ void ASTUAICharacter::OnDeath()
     {
         STUController->BrainComponent->Cleanup();
     }
+}
+
+void ASTUAICharacter::OnHealthChanged(float Health, float HealthDelta)
+{
+    Super::OnHealthChanged(Health, HealthDelta);
+
+    const auto HealthBarWidget = Cast<USTUHealthBarWidget>(HealthWidgetComponent->GetUserWidgetObject());
+    if (!HealthBarWidget)
+    {
+        return;
+    }
+
+    HealthBarWidget->SetHealthPercent(HealthComponent->GetHealthPercent());
+}
+
+void ASTUAICharacter::UpdateHealthWidgetVisibility()
+{
+    if (!GetWorld() ||                             //
+        !GetWorld()->GetFirstPlayerController() || //
+        !GetWorld()->GetFirstPlayerController()->GetPawnOrSpectator())
+    {
+        return;
+    }
+    const auto PlayerLocation = GetWorld()->GetFirstPlayerController()->GetPawnOrSpectator()->GetActorLocation();
+    const auto Distance = FVector::Distance(PlayerLocation, GetActorLocation());
+    HealthWidgetComponent->SetVisibility(Distance < HealthVisibilityDistance, true);
 }
---
+++ Source/ShootThemUp/Private/UI/STUHealthBarWidget.cpp
@@ -0,0 +1,23 @@
+// Shoot Them Up Game, by Pheniex
+
+
+#include "UI/STUHealthBarWidget.h"
+#include "Components/ProgressBar.h"
+
+void USTUHealthBarWidget::SetHealthPercent(float Percent) 
+{
+    if (!HealthProgressBar)
+    {
+        return;
+    }
+
+    const auto HealthBarVisibility = (Percent > PercentVisibilityThreshold || FMath::IsNearlyZero(Percent)) //
+                                         ? ESlateVisibility::Hidden
+                                         : ESlateVisibility::Visible;
+    HealthProgressBar->SetVisibility(HealthBarVisibility);
+
+    const auto HealthBarColor = Percent > PercentColorThreshold ? GoodColor : BadColor;
+    HealthProgressBar->SetFillColorAndOpacity(HealthBarColor);
+
+    HealthProgressBar->SetPercent(Percent);
+}
